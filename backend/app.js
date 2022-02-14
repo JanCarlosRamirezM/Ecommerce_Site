c6c9e0fea3273fafda3486348df72fea382b47ea
JancarlosRamirezM <jancarlosramirez2@gmail.com>
2022-02-13 20:31:02 -0400

    Crear y guardar producto

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 // Import all routes
 const products = require("./routers/product");
 
--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,20 @@
+const Product = require("../models/product");
+
+// --------------------------------------------
+// Create new product => /api/v1/product/new
+// --------------------------------------------
+exports.newProduct = async (req, res, next) => {
+  const product = await Product.create(req.body);
+
+  res.status(201).json({
+    success: true,
+    product,
+  });
+};
+
+// --------------------------------------------
+// Get all products => /api/v1/products
+// --------------------------------------------
 exports.getProducts = (req, res, next) => {
   return res.status(200).json({
     success: true,
