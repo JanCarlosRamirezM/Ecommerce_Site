1ec936bea58a88f873fc6d64c0aac5e445810ed6
JancarlosRamirezM <jancarlosramirez2@gmail.com>
2022-03-28 12:08:09 -0400

    Add New  Update Review

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -7,7 +7,6 @@ const APIFeatures = require("../utils/apiFeatures");
 // Create new product => /api/v1/product/new
 // --------------------------------------------
 exports.newProduct = CatchAsyncErrors(async (req, res, next) => {
-  
   req.body.user = req.user.id;
   const product = await Product.create(req.body);
 
@@ -102,5 +101,47 @@ exports.deleteProduct = CatchAsyncErrors(async (req, res, next) => {
   return res.status(200).json({
     success: true,
     message: "Product is deleted",
+  });
+});
+
+
+// --------------------------------------------
+// Create new Review => /api/v1/product/review
+// --------------------------------------------
+exports.createProductReview = CatchAsyncErrors(async (req, res, next) => {
+  const { rating, comment, productId } = req.body;
+
+  const review = {
+    user: req.user.id,
+    name: req.user.name,
+    rating: Number(rating),
+    comment,
+  };
+ 
+  const product = await Product.findById(productId);
+  const isReviewed = product.reviews.find(
+    (r) => r.user.toString() === review.user.toString()
+  );
+
+  if (isReviewed) {
+    product.reviews.forEach((rev) => {
+      if (rev.user._id.toString() === review.user.toString()) {
+        rev.comment = comment;
+        rev.rating = rating;
+      }
+    });
+  } else {
+    product.reviews.push(review);
+    product.numOfReview = product.reviews.length;
+  }
+
+  product.ratings =
+    product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+    product.reviews.length;
+
+  await product.save({ validateBeforeSave: false });
+
+  return res.status(200).json({
+    success: true,
   });
 });
