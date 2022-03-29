c854b7c5d016904eaec66e01db4c902070f7cf6e
JancarlosRamirezM <jancarlosramirez2@gmail.com>
2022-03-29 16:31:47 -0400

    Create Redux Store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
- 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
- 
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
